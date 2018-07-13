1edea1cebba0077d565b161dcee1f63ee289b1b4
Tao <werewaswas@live.com>
2018-07-13 22:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach( (el) => { console.log(el.name)})
+}
+
+function logDriversByHometown(drivers, hometown) {
+    drivers.forEach( (el) => {
+      if (el.hometown == hometown) { console.log(el.name) }
+    }
+  )
+}
+
+function driversByRevenue(drivers) {
+  const driversCopy = [...drivers]
+  return driversCopy.sort((a,b) => {return a.revenue - b.revenue})
+}
+
+function driversByName(drivers) {
+  const driversCopy = [...drivers]
+  return driversCopy.sort((a,b) => {return a.name.localeCompare(b.name)})
+}
+
+function totalRevenue(drivers) {
+  function callback(agg, el, i, arr) {
+    return agg + el.revenue;
+  }
+  drivers.reduce(callback, 0)
+}
